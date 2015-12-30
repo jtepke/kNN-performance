056d62ec93ee19a6a2406039b7055013f656e19f
jtepke <peter.silie42@gmx.de>
2015-12-30 14:41:54 +0100

    New TestRunner scenario generates random points

--- src/TestRunner.cpp
+++ src/TestRunner.cpp
@@ -1,11 +1,13 @@
 #include "grid/MBR.h"
 #include "grid/Grid.h"
-
 #include <iostream>
 #include <string>
 #include <cstdio>
+#include <cstdlib>
 
-#define NUMBER_OF_POINTS 39999
+#include "util/FileHandler.h"
+#include "util/RandomPointGenerator.h"
+
 #define MAX_COORD 1000.0
 #define MIN_COORD -1000.0
 
@@ -14,36 +16,46 @@ double fRand(double fMin, double fMax) {
 	return fMin + f * (fMax - fMin);
 }
 
-int main(int argc, char** agrv) {
+int main(int argc, char** argv) {
 	std::cout << "Spatial Grid Test Runner...\t" << "running..." << std::endl;
 
-//	double points[9] = { 1.2, 1.3, 1.4, 1.3, 3.2, 4.4, 5.2, 8.3, 10.4 };
-//	Grid* g = new Grid(3, points, 9);
+	int NUMBER_OF_POINTS = std::stoi(argv[1]);
+	int DIMENSION = std::stoi(argv[2]);
+	int NUMBER_OF_COORDS = NUMBER_OF_POINTS * DIMENSION;
+
+	double mbrCoords[] = { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 4.0, 4.0, 4.0, 4.0,
+			4.0, 4.0 };
+
+	MBR m = MBR::buildMBR(mbrCoords, 12, 6);
+
+	m.to_stream(std::cout);
+
+	RandomPointGenerator g(12345);
+
+	double* randomPtsOut = g.generatePoints(NUMBER_OF_POINTS,
+			RandomPointGenerator::GAUSS_CLUSTER, m, 5.0, 2.0, 4);
+//	double* randomPtsIn = new double[NUMBER_OF_COORDS];
+
+//	for (int i = 0; i < NUMBER_OF_COORDS; i++) {
+//		double randNr = fRand(MIN_COORD, MAX_COORD);
+//		randomPtsOut[i] = randNr;
+//	}
+
+	PointContainer p(DIMENSION);
+	p.add(randomPtsOut, NUMBER_OF_COORDS);
+	p.to_stream(std::cout);
+
+//	FileHandler::writePointsToFile("testPts.bin", randomPtsOut,
+//			NUMBER_OF_COORDS);
+//
+//	delete[] (randomPtsOut);
+//	FileHandler::readPointsFromFile("testPts.bin", randomPtsIn,
+//			NUMBER_OF_POINTS, DIMENSION);
+//	Grid* g = new Grid(DIMENSION, randomPtsIn, NUMBER_OF_COORDS);
+//	delete[] (randomPtsIn);
+//
 //	g->to_stream(std::cout);
-
-	double* randomPtsOut = new double[NUMBER_OF_POINTS];
-	double* randomPtsIn = new double[NUMBER_OF_POINTS];
-
-	for (std::size_t i = 0; i < NUMBER_OF_POINTS; i++) {
-		double randNr = fRand(MIN_COORD, MAX_COORD);
-		randomPtsOut[i] = randNr;
-
-//		std::cout << std::to_string(randNr) << std::endl;
-	}
-
-	std::FILE* fout = std::fopen("testPts.bin", "wb");
-	std::fwrite(randomPtsOut, sizeof(double), NUMBER_OF_POINTS, fout);
-	std::fclose(fout);
-
-	delete(randomPtsOut);
-	std::FILE* fin = std::fopen("testPts.bin", "rb");
-	std::fread(randomPtsIn, sizeof(double), NUMBER_OF_POINTS, fin);
-	std::fclose(fin);
-
-	Grid* g = new Grid(3, randomPtsIn, NUMBER_OF_POINTS);
-//	delete(randomPtsIn);
-//	delete(g);
-//	g->to_stream(std::cout);
+//	delete (g);
 
 	std::cout << "END" << std::endl;
 	return 0;
