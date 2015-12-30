add10681d6e74a2cf8300f7c2d4646c47166ad29
jtepke <peter.silie42@gmx.de>
2015-12-30 14:51:05 +0100

    Add random generator for n-dimensional point data

---
+++ src/util/RandomPointGenerator.cpp
@@ -0,0 +1,96 @@
+/*
+ * RandomGenerator.cpp
+ *
+ *  Created on: Dec 19, 2015
+ *      Author: jan
+ */
+
+#include "../util/RandomPointGenerator.h"
+#include <stdexcept>
+#include <cassert>
+
+void RandomPointGenerator::initUniform(MBR& m, std::size_t dimension) {
+	uniform_ = new std::uniform_real_distribution<double> *[dimension];
+
+	for (std::size_t i = 0; i < dimension; i++) {
+		auto min = m.getLowerPoint()[i];
+		auto max = m.getUpperPoint()[i];
+		uniform_[i] = new std::uniform_real_distribution<double>(min, max);
+	}
+}
+
+void RandomPointGenerator::initGauss(double mean, double stddev) {
+	gauss_ = new std::normal_distribution<double>*[1];
+	gauss_[0] = new std::normal_distribution<double>(mean, stddev);
+}
+
+void RandomPointGenerator::initGaussCluster(std::size_t numberOfClusters,
+		double stddev) {
+	gauss_ = new std::normal_distribution<double>*[numberOfClusters];
+
+	for (std::size_t i = 0; i < numberOfClusters; i++) {
+		auto mean = (*uniform_[i])(randEngine_);
+		gauss_[i] = new std::normal_distribution<double>(mean, stddev);
+	}
+}
+
+void RandomPointGenerator::genUniformPts(double * randPts,
+		std::size_t numberOfPoints, std::size_t dimension) {
+
+	for (std::size_t i = 0; i < numberOfPoints; i++) {
+		for (std::size_t j = 0; j < dimension; j++) {
+			randPts[(i * dimension) + j] = (*uniform_[j])(randEngine_);
+		}
+	}
+}
+
+void RandomPointGenerator::genGaussPts(double * randPts,
+		std::size_t numberOfPoints, std::size_t dimension,
+		std::size_t numberOfClusters, MBR& m) {
+	std::size_t clusterID = 0;
+	std::size_t coordIndex = 0;
+
+	for (std::size_t point = 0; point < numberOfPoints; point++) {
+		clusterID = point % numberOfClusters;
+		for (std::size_t dim = 0; dim < dimension; dim++) {
+			coordIndex = (point * dimension) + dim;
+			randPts[coordIndex] = (*gauss_[clusterID])(randEngine_);
+		}
+	}
+}
+
+double * RandomPointGenerator::generatePoints(std::size_t numberOfPoints,
+		DISTRIBUTION distrib, MBR& mbr, double mean, double stddev,
+		int numberOfClusters) {
+	assert(!mbr.empty());
+
+	std::size_t dimension = mbr.getLowerPoint().dimension();
+	std::size_t numberOfCoordinates = numberOfPoints * dimension;
+
+	double * randPoints = new double[numberOfCoordinates];
+
+	switch (distrib) {
+	case GAUSS_CLUSTER:
+		initUniform(mbr, dimension);
+		initGaussCluster(numberOfClusters, stddev);
+		genGaussPts(randPoints, numberOfPoints, dimension, numberOfClusters,
+				mbr);
+		break;
+	case GAUSS:
+		initGauss(mean, stddev);
+		genGaussPts(randPoints, numberOfPoints, dimension, 1,
+						mbr);
+		break;
+	case UNIFORM:
+		initUniform(mbr, dimension);
+		genUniformPts(randPoints, numberOfPoints, dimension);
+		break;
+	default:
+		//this should never happen...
+		throw std::runtime_error("Cannot handle distribution: " + distrib);
+	}
+
+	return randPoints;
+
+}
+
---
+++ src/util/RandomPointGenerator.h
@@ -0,0 +1,39 @@
+#ifndef UTIL_RANDOMPOINTGENERATOR_H_
+#define UTIL_RANDOMPOINTGENERATOR_H_
+
+#include <random>
+#include "../grid/MBR.h"
+
+class RandomPointGenerator {
+private:
+	std::random_device randDevice_;
+	std::default_random_engine randEngine_;
+	std::uniform_real_distribution<double>** uniform_;
+	std::normal_distribution<double>** gauss_;
+
+	void initUniform(MBR& m, std::size_t dimension);
+	void initGauss(double mean, double stddev);
+	void initGaussCluster(std::size_t dimension, double stddev);
+	void genUniformPts(double * randPts, std::size_t numberOfPoints,
+			std::size_t dimension);
+	void genGaussPts(double * randPts, std::size_t numberOfPoints,
+			std::size_t dimension, std::size_t numberOfClusters, MBR& m);
+public:
+	RandomPointGenerator() :
+			randEngine_(randDevice_()) {
+
+	}
+
+	RandomPointGenerator(unsigned seed) :
+			randEngine_(seed) {
+	}
+
+	enum DISTRIBUTION {
+		UNIFORM, GAUSS, GAUSS_CLUSTER
+	};
+
+	double * generatePoints(std::size_t numberOfPoints, DISTRIBUTION d, MBR& m,
+			double mean = 0.0, double stddev = 1.0, int numberOfClusters = 1);
+};
+
+#endif /* UTIL_RANDOMPOINTGENERATOR_H_ */
