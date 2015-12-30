e4d51962dd727189a4a360a524fac392acd9ec07
jtepke <peter.silie42@gmx.de>
2015-12-30 14:43:00 +0100

    Move Representable to util/

---
+++ src/util/Representable.cpp
@@ -0,0 +1,4 @@
+#include "Representable.h"
+
+
+
---
+++ src/util/Representable.h
@@ -0,0 +1,18 @@
+#ifndef REPRESENTABLE_H_
+#define REPRESENTABLE_H_
+
+#include <string>
+#include <ostream>
+#include <iostream>
+
+class Representable {
+public:
+	Representable() {
+	}
+	virtual ~Representable() {
+
+	}
+	virtual void to_stream(std::ostream& os) = 0;
+};
+
+#endif /* REPRESENTABLE_H_ */
