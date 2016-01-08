2c067bddf8bbf0e568a3325cc2f9fe19100387ab
Jan Tepke <jan.tepke@sap.com>
2016-01-08 17:26:58 +0100

    Add template for Grid test

---
+++ test/Grid_test.cpp
@@ -0,0 +1,20 @@
+#include "gtest/gtest.h"
+#include "grid/Grid.h"
+
+#include "cmath"
+
+class GridTest: public ::testing::Test {
+protected:
+
+	GridTest() {
+
+	}
+
+	virtual void SetUp() {
+	}
+	virtual void TearDown() {
+	}
+};
+
+TEST_F(GridTest, kNN_works_properly) {
+}
