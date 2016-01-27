0c3bf16a6d33eea95e72882182ad8675c76068a2
Jan Tepke <jan.tepke@sap.com>
2016-01-27 14:20:48 +0100

    Alter grid performance test

--- performance-test/grid_performance.cpp
+++ performance-test/grid_performance.cpp
@@ -12,10 +12,10 @@
 
 typedef std::chrono::milliseconds mili_sec;
 
-std::pair<long, std::vector<long>> testGrid(unsigned dimension,
-		std::vector<unsigned>& ks, unsigned numberOfPoints,
-		std::size_t cellFillOptimum, PointAccessor* query,
-		PointContainer& points) {
+std::pair<long, long> testGrid(unsigned dimension, unsigned k,
+		unsigned numberOfPoints, std::size_t cellFillOptimum,
+		PointContainer& queries, PointContainer& points) {
+
 	auto start_grid_build = std::chrono::system_clock::now();
 	Grid grid(dimension, points.data(), numberOfPoints * dimension,
 			cellFillOptimum);
@@ -23,64 +23,54 @@ std::pair<long, std::vector<long>> testGrid(unsigned dimension,
 			< mili_sec
 			> (std::chrono::system_clock::now() - start_grid_build).count());
 
-	std::vector<long> kRuntime;
-
-	for (auto k : ks) {
-		auto start_knn = std::chrono::system_clock::now();
-		grid.kNearestNeighbors(k, query);
-		long knn_duration = static_cast<long>(std::chrono::duration_cast
-				< mili_sec
-				> (std::chrono::system_clock::now() - start_knn).count());
-
-		kRuntime.push_back(knn_duration);
+	auto start_knn = std::chrono::system_clock::now();
+	for (std::size_t q_idx = 0; q_idx < queries.size(); ++q_idx) {
+		auto&& q = queries[q_idx];
+		grid.kNearestNeighbors(k, &q);
 	}
 
-	return std::make_pair(grid_build_duration, kRuntime);
+	long knn_duration = static_cast<long>(std::chrono::duration_cast < mili_sec
+			> (std::chrono::system_clock::now() - start_knn).count());
+
+	return std::make_pair(grid_build_duration, knn_duration);
 }
 
 int main(int argc, char** argv) {
 
-	long SEED = 12345l;
+	long SEED = 62345l;
 	const unsigned DIMENSION = 3;
-	unsigned NUMBER_OF_TEST_POINTS = 100000;
-	unsigned K = 1000;
+	unsigned NUMBER_OF_TEST_POINTS = 100000000;
+	unsigned NUMBER_OF_QUERIES = 1000;
+	unsigned K = 50;
 
 	RandomPointGenerator rg(SEED);
 	unsigned NUMBER_OF_MBR_COORDINATES = 2 * DIMENSION;
 	double mbrCoords[] = { -100.0, -100.0, -100.0, 100.0, 100.0, 100.0 };
+	double query_mbrCoords[] = { -99.0, -99.0, -99.0, 99.0, 99.0, 99.0 };
 
-	MBR m = MBR(DIMENSION);
-	m = m.createMBR(mbrCoords, NUMBER_OF_MBR_COORDINATES);
+	MBR mbr = MBR(DIMENSION);
+	MBR query_mbr = MBR(DIMENSION);
+	mbr = mbr.createMBR(mbrCoords, NUMBER_OF_MBR_COORDINATES);
+	query_mbr = mbr.createMBR(query_mbrCoords, NUMBER_OF_MBR_COORDINATES);
+
 	PointContainer points = rg.generatePoints(NUMBER_OF_TEST_POINTS,
-			RandomPointGenerator::UNIFORM, m);
+			RandomPointGenerator::UNIFORM, mbr);
 
-	double queryCoords[DIMENSION] = { 1.0, 1.0, 1.0 };
-	PointArrayAccessor query(queryCoords, 0, DIMENSION);
+	PointContainer queries = rg.generatePoints(NUMBER_OF_QUERIES,
+			RandomPointGenerator::UNIFORM, query_mbr);
 
 	std::cout << "#### Starting Grid Performance Test ###" << std::endl;
-
-	std::vector<unsigned> ks;
-	long sum = 0;
-	for (unsigned k = 1; k < (K + 2)/4; k += 10) {
-		ks.push_back(k);
-	}
 
 	for (std::size_t cellFillOpt = 10; cellFillOpt <= 1000; cellFillOpt +=
 			100) {
 
-		auto res_pair = testGrid(DIMENSION, ks, NUMBER_OF_TEST_POINTS,
-				cellFillOpt, &query, points);
+		auto res_pair = testGrid(DIMENSION, K, NUMBER_OF_TEST_POINTS,
+				cellFillOpt, queries, points);
 
 		std::cout << "build time: " << res_pair.first << std::endl;
+		std::cout << "kNN lookup time: " << res_pair.second << std::endl;
 		std::cout << "cell-fill-optimum: " << cellFillOpt << std::endl;
-		sum = 0;
-
-		for (auto kRuntime : res_pair.second) {
-			sum += kRuntime;
-		}
-
-		std::cout << "sum of [" << ks.size() << "] kNN  rumtimes: " << sum
-				<< std::endl;
+		std::cout << "k: " << K << std::endl;
 	}
 	std::cout << "#### Grid Performance Test Succeeded###" << std::endl;
 	return EXIT_SUCCESS;
