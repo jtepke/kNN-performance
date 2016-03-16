cd67da4fec9ce67a1b867c8f635240b27b1d8d87
jtepke <peter.silie42@gmx.de>
2016-03-16 15:21:47 +0100

    Add comments to Grid.h
    * Improves and corrects comments as well.

--- src/grid/Grid.h
+++ src/grid/Grid.h
@@ -13,45 +13,48 @@
 
 class Grid: public Representable, public KnnProcessor<PointVectorAccessor> {
 public:
-	/** dimension of the grid space */
+	/** Dimension of the grid space. */
 	const std::size_t dimension_;
-	/** minimum bounding hyperrectangle around the inserted point cloud */
+	/** Minimum bounding hyperrectangle around the inserted point cloud. */
 	MBR mbr_;
-	/** number of points in the grid */
+	/** Number of points stored in the grid. */
 	const std::size_t numberOfPoints_;
-	/** width of the grid in each dimension */
+	/** Grid width in each dimension. */
 	const std::vector<double> gridWidthPerDim_;
-	/** number if cells per row in each dimension */
+	/** Number of cells per row in each dimension. */
 	const std::vector<std::size_t> cellsPerDimension_;
-	/** product of cells up to dimension, necessary for several numerical calculations*/
+	/** Product of cells up to dimension,
+	 * necessary for several numerical calculations. */
 	const std::vector<std::size_t> productOfCellsUpToDimension_;
-	/** the grid is a vector of buckets containing points */
+	/** The grid is modeled as a vector of buckets containing points. */
 	std::vector<PointContainer> grid_;
-	/** locks for multi-threaded insert operation*/
+	/** Locks for multi-threaded insert operation. */
 	std::vector<std::mutex*> insertLocks_;
-	/** we assume this the optimal number points per cell */
+	/** We assume this the optimal number points per cell.
+	 *  Tests revealed this thresholds works good for k < 1000. */
 	static const std::size_t CELL_FILL_OPTIMUM_DEFAULT = 200;
-	/** Default value for max number of insert threads*/
+	/** Default value for max number of insert threads. */
 	static const unsigned MAX_NUMBER_OF_THREADS_DEFAULT = 20;
-	/** Default value for max points to be inserted single-threaded*/
+	/** Default value for max points to be inserted single-threaded. */
 	static const unsigned THREAD_LOAD_DEFAULT = 100000000;
-	/** Maximum number of insert threads*/
+	/** Maximum number of insert threads. */
 	unsigned maxNumberOfThreads_;
-	/** Threshold to switch from single- to multi-threaded*/
+	/** Threshold to switch from single- to multi-threaded. */
 	unsigned threadLoad_;
 	/** Create an MBR around the grid points. */
 	static MBR initGridMBR(double * coordinates, std::size_t dimension,
 			std::size_t size);
-	/** Determine ideal cell size for given value k */
-	/** Caveat: Only works properly with uniformly distributed points and k < 1000-ish*/
+	/** Determine ideal cell size for given value k. */
+	/** Caveat: Only works properly with uniformly distributed points
+	 * and k < 1000-ish. */
 	static std::size_t determineCellSize(unsigned k);
 	/** Insert a set of points into the grid. */
 	void insert(double * coordinates, std::size_t size);
 	/** Insert single point into grid. */
 	void insert(double * point, bool isMultiThreaded);
-	/** insert, using locks*/
+	/** Insert, using locks. */
 	void insertMultiThreaded(double* coordinates, std::size_t size);
-	/** Calculates gridWith per dimension. */
+	/** Calculates grid width per dimension. */
 	const std::vector<double> widthPerDimension();
 	/** Returns vector containing number of cells per dimension. */
 	//TODO: make static
@@ -68,11 +71,12 @@ public:
 	void allocPointContainers();
 
 	/** kNN utility methods: */
-	/** Returns squared distance to query point.*/
+	/** Returns squared distance to query point. */
 	double findNextClosestCellBorder(PointAccessor* query, int kNNiteration);
-	/** Return a list of cell numbers for certain kNN iteration.*/
+	/** Return a list of cell numbers for certain kNN iteration. */
 	std::vector<unsigned> getHyperSquareCellEnvironment(int kNN_iteration,
 			unsigned queryCell, std::vector<unsigned>& cartesianQueryCoords);
+	/** Returns Cartesian coordinate for a given cell number. */
 	std::vector<unsigned> getCartesian(unsigned cellNumber);
 	void initMinAndMax(std::vector<int>& min, std::vector<int>& max,
 			int kNN_iteration,
@@ -84,12 +88,15 @@ public:
 
 //public:
 	Grid(const std::size_t dimension, double * coordinates, std::size_t size,
-			std::size_t cellFillOptimum = Grid::CELL_FILL_OPTIMUM_DEFAULT, unsigned maxNumberOfThreads = MAX_NUMBER_OF_THREADS_DEFAULT, unsigned threadLoad = THREAD_LOAD_DEFAULT) :
+			std::size_t cellFillOptimum = Grid::CELL_FILL_OPTIMUM_DEFAULT,
+			unsigned maxNumberOfThreads = MAX_NUMBER_OF_THREADS_DEFAULT,
+			unsigned threadLoad = THREAD_LOAD_DEFAULT) :
 			dimension_(dimension), mbr_(
 					Grid::initGridMBR(coordinates, dimension, size)), numberOfPoints_(
 					size / dimension), gridWidthPerDim_(widthPerDimension()), cellsPerDimension_(
 					calculateCellsPerDimension(cellFillOptimum)), productOfCellsUpToDimension_(
-					initProductOfCellsUpToDimension(dimension)), maxNumberOfThreads_(maxNumberOfThreads), threadLoad_(threadLoad) {
+					initProductOfCellsUpToDimension(dimension)), maxNumberOfThreads_(
+					maxNumberOfThreads), threadLoad_(threadLoad) {
 
 		allocPointContainers();
 		insert(coordinates, size);
